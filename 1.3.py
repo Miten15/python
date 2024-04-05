104b70cb4dfaeb3adce8adade2af206d166e4684
Miten <miten2006@gmail.com>
2024-04-05 09:02:57 +0530

    find the area and the perimeter of the square

---
+++ 1.3.py
@@ -0,0 +1,13 @@
+#find the area and the perimeter of the squar
+
+def area_of_square(side_length):
+    return side_length  * side_length
+
+def perimeter_of_square(side_length):
+    return 4 * side_length
+
+# asking for inputs
+side_length = float(input("Enter length: "))
+
+print("The area of the square is:", area_of_square(side_length))
+print("The perimeter of the square is:", perimeter_of_square(side_length))
