b6a47da980cfa4d8c4a52dad6efa4a7a6cb5b76d
Miten <miten2006@gmail.com>
2024-04-05 08:49:47 +0530

    Find Squar Root of number

---
+++ 1.1.py
@@ -0,0 +1,9 @@
+#Find Squar Root of number
+
+import math
+
+def squarRoot(num):
+ return  math.sqrt(num)
+
+num =  float(input("Enter a number: "))
+print("The square root of", num, "is", squarRoot(num))
