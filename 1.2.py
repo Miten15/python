1099379f8f5640e0e5e607ff23d5a857543f3c2e
Miten <miten2006@gmail.com>
2024-04-05 08:52:46 +0530

    find area of the rectangle

---
+++ 1.2.py
@@ -0,0 +1,8 @@
+#find area of the rectangle
+
+def  find_area(length,breath):
+    return length*breath
+
+length=float(input("Enter the length: "))
+breath=float(input("Enter the  breadth: "))
+print("The Area is ",find_area(length,breath))
