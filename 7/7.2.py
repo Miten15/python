d9852cc106a8a28910307a8d785cc8bed4d3d39a
Miten <miten2006@gmail.com>
2024-04-06 19:01:09 +0530

    wap to demonstrate math built-in functions

---
+++ 7/7.2.py
@@ -0,0 +1,12 @@
+#wap to demonstrate math built-in functions
+import math
+
+num = int(input("Enter a number: "))
+
+print(abs(num))
+print(math.sqrt(num))  # square root of the num
+print(math.factorial(num))
+print(math.fabs(num))
+print(math.floor(num))   
+print(math.log(num))
+print(math.log10(num))  
