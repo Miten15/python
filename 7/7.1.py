8270c2bb4c1d4c288578d3a03c6ab983c504295d
Miten <miten2006@gmail.com>
2024-04-06 18:56:27 +0530

    #wap to demonstrate string built-in functions

---
+++ 7/7.1.py
@@ -0,0 +1,9 @@
+#wap to demonstrate string built-in functions
+
+str = input("Enter the string:")
+
+print(str.capitalize())
+print(str.lower())
+print(str.upper())
+print(str.split())
+print(str.find('a'))
