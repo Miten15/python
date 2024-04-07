8d9de376b771c594a45ab8ae1a37bb8b7c117077
Miten <miten2006@gmail.com>
2024-04-07 16:10:34 +0530

    wap to find the area and the circumference of  a circle

--- 9/9.py
+++ 9/9.py
@@ -3,7 +3,8 @@
 import math
 
 r = float(input("Enter the radius of the crile:"))
-area=math.pi * r**2 #'**' is used to make the number ^2 
+area=math.pi * r**2 #'**' is used to make the number ^2.
+
 circumference=2* math.pi * r
 print ("The area of the circle is", area)
 print ("The circumference of the circle is", circumference)
