fa87a9ae092cc28e941bfb2c1d274b6947df9507
Miten <miten2006@gmail.com>
2024-04-07 17:03:33 +0530

    wap to concatenate two strings

---
+++ 10/10.2.py
@@ -0,0 +1,9 @@
+#wap to concatenate two strings
+
+def  concat_strings(str1, str2):
+    return str1 + str2
+
+str1 = input("enter your string number 1:")
+str2 = input("enter your string number 1:")
+
+print("Heres ur concatenated string:", concat_strings(str1, str2))
