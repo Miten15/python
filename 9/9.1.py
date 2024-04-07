2715201fe0b5805b1ba7fdafa836156fe18df5fd
Miten <miten2006@gmail.com>
2024-04-07 16:21:38 +0530

    wap to print the calendar of the month using calendar module

---
+++ 9/9.1.py
@@ -0,0 +1,6 @@
+#wap to print the calendar of the month using calendar module
+import calendar
+
+year = 2011
+month = int(input("Enter the month u wana print in numbers:"))
+print(calendar.month(year,month))
