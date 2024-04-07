3c695a57102d24d49e1b255ad14f8b5c712d8286
Miten <miten2006@gmail.com>
2024-04-07 16:10:09 +0530

    takes an string from user and chec

---
+++ 9/9.py
@@ -0,0 +1,9 @@
+#wap to find the area and the circumference of  a circle
+
+import math
+
+r = float(input("Enter the radius of the crile:"))
+area=math.pi * r**2 #'**' is used to make the number ^2 
+circumference=2* math.pi * r
+print ("The area of the circle is", area)
+print ("The circumference of the circle is", circumference)
